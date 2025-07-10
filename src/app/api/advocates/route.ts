953e3f54b71e95710b9a8e7f0e6740b043a1b56f
Hannah McDonald <83371610+hannahnmcdonald@users.noreply.github.com>
2025-07-09 21:22:27 -0400

    add indexes to first, lastname and city on the table for faster and more efficient searching

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,27 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { ilike, or, desc } from "drizzle-orm";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const search = searchParams.get("search")?.trim();
 
-  const data = advocateData;
+  let query = db
+    .select()
+    .from(advocates)
+    .orderBy(desc(advocates.created_at));
+
+    if (search) {
+      query = query.where(
+        or(
+          ilike(advocates.first_name, `%${search}`),
+          ilike(advocates.last_name, `%${search}`),
+          ilike(advocates.city, `%${search}`)
+        )
+      )
+    }
+
+  const data = await db.select().from(advocates);
 
   return Response.json({ data });
 }
